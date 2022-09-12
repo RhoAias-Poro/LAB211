0d12bf86df173732478f03687d295ecb2abb9bf9
RhoAias-Poro <85359791+RhoAias-Poro@users.noreply.github.com>
2022-09-12 10:26:44 +0700

    update cmt

--- J1.S.P0002/src/bo/SelectionSort.java
+++ J1.S.P0002/src/bo/SelectionSort.java
@@ -3,31 +3,36 @@ package bo;
 import utils.ArrayUtils;
 
 public class SelectionSort {
-    private int[] originalArray;
-    private int arrayLength;
+    private int[] originalArray; //to store originalArray
+    private int arrayLength; // store length of the array
 
+    //constructor
     public SelectionSort(int[] originalArray, int arrayLength) {
         this.arrayLength = arrayLength;
         this.originalArray = originalArray;
     }
 
+    //Implement Selection Sort
     private int[] sort() {
-        int[] cloneArray = ArrayUtils.cloneIntArray(originalArray);
-        int index = 0;
+        int[] cloneArray = ArrayUtils.cloneIntArray(originalArray); // create a clone of Original Array
+        int index = 0; // to store the min value index
         for (int i = 0; i < arrayLength; i++) {
-            int min = cloneArray[i];
+            int min = cloneArray[i]; // set it to the first element in the search
             for (int j = i+1; j < arrayLength; j++) {
-                if (min > cloneArray[j])
+                if (min > cloneArray[j]) // if any next element is < min
                 {
                     min = cloneArray[j];
                     index = j;
                 }
             }
+            // after the loop, min is the smallest in the loop i
+            // after that swap them with the first element of the loop
             if(min < cloneArray[i]) ArrayUtils.swapFromAnArray(cloneArray, index, i);
         }
         return cloneArray;
     }
 
+    //print the result
     public void displayResult() {
         System.out.print("Unsorted array:");
         ArrayUtils.printIntArray(originalArray, arrayLength);
--- J1.S.P0002/src/ui/Main.java
+++ J1.S.P0002/src/ui/Main.java
@@ -7,9 +7,9 @@ import utils.IntegerUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int arrayLength = IntegerUtils.inputNumberOfArray();
-        int[] arrayNeedSort = ArrayUtils.createRandomIntArray(arrayLength);
-        SelectionSort sort = new SelectionSort(arrayNeedSort, arrayLength);
-        sort.displayResult();
+        int arrayLength = IntegerUtils.inputNumberOfArray(); // take length of the array from user input
+        int[] arrayNeedSort = ArrayUtils.createRandomIntArray(arrayLength); //create random array based on the length of the array
+        SelectionSort sort = new SelectionSort(arrayNeedSort, arrayLength);// SelectionSort obj
+        sort.displayResult();// display the result after sort
     }
 }
